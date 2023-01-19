70de890b5344464b02eb036b8b62a3044a1bb871
lnyagyaeva <littlepoppet@mail.ru>
2023-01-19 22:48:25 +0300

    parametrize test added

--- test/test_dividor_negative.py
+++ test/test_dividor_negative.py
@@ -1,5 +1,5 @@
 import pytest
-from src.library_1 import dividor
+from src.library import dividor
 
 def test_library_dividor_negative():
     assert dividor(-4,2) == -2, 'Division result of 10 and 1 is not equal to 10'
